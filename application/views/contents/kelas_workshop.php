41fbd88559e335c8f6bdf46f829d8a52c094a019
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-08-28 02:34:36 +0700

    add bentuk pilih kelas workshop

--- application/views/contents/kelas_workshop.php
+++ application/views/contents/kelas_workshop.php
@@ -1,52 +1,18 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card">
-                <div class="content">
-                    <div class="row">
-                        <div class="col-xs-5">
-                            <div class="icon-big icon-warning text-center">
-                                <i class="ti-server"></i>
-                            </div>
-                        </div>
-                        <div class="col-xs-7">
-                            <div class="numbers">
-                                <p>Kelas</p>
-                                Presentasi
-                            </div>
-                        </div>
-                    </div>
-                    <div class="footer">
-                        <hr />
-                        <div class="stats">
-                            <div class="col-xs-12">
-                              <div class="col-xs-6">
-                                <i class="ti-ruler-pencil"></i> 10/49
-                              </div>
-                              <div class="col-xs-6">
-                                <a href="#" class="btn btn-success btn-xs btn-fill btn-wd">Pilih Kelas</a>
-                              </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <!-- contoh kelas penuh -->
         <div class="col-lg-4 col-md-4 col-sm-6">
             <div class="card">
                 <div class="content">
                     <div class="row">
-                        <div class="col-xs-5">
-                            <div class="icon-big icon-warning text-center">
-                                <i class="ti-server"></i>
-                            </div>
+                        <div class="col-xs-4">
+                            <img class="img-voting" src="https://user-images.githubusercontent.com/10141928/29752968-f927f8bc-8b91-11e7-8593-13c99ef6c173.png" alt="kelas_workshop">
                         </div>
-                        <div class="col-xs-7">
+                        <div class="col-xs-8">
                             <div class="numbers">
                                 <p>Kelas</p>
-                                Presentasi
+                                Presentation Skill
                             </div>
                         </div>
                     </div>
@@ -58,7 +24,7 @@
                                 <i class="ti-ruler-pencil"></i> Kelas Penuh
                               </div>
                               <div class="col-xs-6">
-                                <a href="#" class="btn btn-danger btn-xs btn-fill btn-wd">Penuh</a>
+                                <a href="#" class="btn btn-danger btn-xs btn-fill btn-wd btn-block">Penuh</a>
                               </div>
                             </div>
                         </div>
