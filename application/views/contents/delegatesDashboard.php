39da83d480f4c12c2853e795c9d1528b18ba3940
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-09-05 16:55:41 +0700

    update dashboard delegates

--- application/views/contents/delegatesDashboard.php
+++ application/views/contents/delegatesDashboard.php
@@ -56,6 +56,29 @@
 								</div>
 							</div>
 						</li>
+						<li>
+							<div class="row">
+								<div class="col-xs-3">
+									<div class="avatar">
+										<img src="https://user-images.githubusercontent.com/10141928/29959370-2ac6cfaa-8f21-11e7-908a-f400599369fe.png" alt="Circle Image" class="img-circle img-no-padding img-responsive" alt="Pilih Komunitas Favorit">
+									</div>
+								</div>
+								<div class="col-xs-6">
+									Pilih Makanan
+									<br>
+                  <?php if ($user->id_makan == !NULL) {
+                    echo '<span class="text-success"><small><i class="fa fa-check-circle"></i>Sudah Dilakukan</small></span>';
+                  }
+                  else {
+                    echo '<span class="text-danger"><small><i class="fa fa-info-circle"></i>Belum Dilakukan</small></span>';
+                  } ?>
+								</div>
+
+								<div class="col-xs-3 text-right">
+									<a href="<?=base_url();?>members/menu_makan" class="btn btn-sm btn-success btn-icon"><i class="fa fa-eye"></i></a>
+								</div>
+							</div>
+						</li>
 					</ul>
 				</div>
 			</div>
