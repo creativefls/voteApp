3f34bc63107013ae093f1f003a3d6ba179434e24
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-09-06 21:45:55 +0700

    benahi view detail komunitas

--- application/views/contents/detail/d_komunitas.php
+++ application/views/contents/detail/d_komunitas.php
@@ -6,19 +6,18 @@
 					<h4 class="title"><?=$info['nama_komunitas'];?></h4>
 					<p class="category">9 September 2017</p>
 				</div>
+				<hr>
         <div class="content">
-							<div class="typo-line">
-									<p>
 										<figure>
 										  <img class="img-responsive" src="<?=$info['gambar_logo'];?>" alt="<?=$info['nama_komunitas'];?>">
                       <figcaption class="text-center"><?=$info['nama_komunitas'];?></figcaption>
-										</figure>
-                    <br>
+										</figure><hr>
+									<p class="description text-left" style="padding:1.2em;">
                     <?=$info['deskripsi'];?>
 									</p>
-							</div>
 
           <div class="text-center">
+						<a href="<?=base_url();?>members/voting_komunitas" class="btn btn-warning btn-sm btn-fill"><i class="ti-search"></i> Lainya</a>
             <a href="<?=base_url();?>members/vote-komunitas/<?=$info['id_komunitas'];?>" class="btn btn-success btn-sm btn-fill"><i class="ti-heart"></i> VOTE</a>
           </div>
 
